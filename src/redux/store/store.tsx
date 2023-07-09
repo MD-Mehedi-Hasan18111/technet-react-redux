7c3575e7681564fbf34b095d8763904366820e43
MD Mehedi Hasan <md@bursement.com>
2023-07-09 12:18:39 +0600

    RTK Query added

--- src/redux/store/store.tsx
+++ src/redux/store/store.tsx
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../features/cart/cartSlice';
 import productReducer from '../features/products/productSlice';
+import { api } from '../api/apiSlice';
 
 export const store = configureStore({
   reducer: {
+    [api.reducerPath]: api.reducer,
     cart: cartReducer,
     products: productReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
